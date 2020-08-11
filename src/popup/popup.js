cedefb98fadf3efb071a356c652bd613cfab6711
David Gonzalez <davidjgonzalez@gmail.com>
2020-08-10 20:13:21 -0400

    Web Components POC

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,16 +1,23 @@
 "use strict";
 
-import "@spectrum-css/vars/dist/spectrum-global.css";
-import "@spectrum-css/vars/dist/spectrum-medium.css";
-import "@spectrum-css/vars/dist/spectrum-light.css";
-import "@spectrum-css/page/dist/index-vars.css";
-import "@spectrum-css/button/dist/index-vars.css";
+import "@spectrum-web-components/theme/sp-theme.js";
+import "@spectrum-web-components/theme/theme-light.js";
+import "@spectrum-web-components/theme/scale-medium.js";
+import "@spectrum-web-components/action-menu/sp-action-menu.js";
+import "@spectrum-web-components/menu/sp-menu.js";
+import "@spectrum-web-components/menu/sp-menu-item.js";
+
 import "@spectrum-css/alert/dist/index-vars.css";
+
+import '@spectrum-web-components/icon/sp-icon.js';
+import '@spectrum-web-components/action-menu/sp-action-menu.js';
+import '@spectrum-web-components/button/sp-button.js';
+import '@spectrum-web-components/button/sp-clear-button.js';
+import '@spectrum-web-components/button/sp-action-button.js';
 
 import "./popup.css";
 
 import moment from 'moment';
-
 import OPTIONS from "../constants";
 
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -29,7 +36,7 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 
         let ul = document.getElementById("metadata");
 
-        ul.innerHTML += `
+        ul.innerHTML = `
           ${getSection("Links", [
             getJira(response.kt),
             getCorpGitEdit(response.gitEdit),
@@ -41,7 +48,7 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             ),
           ])}
 
-          ${getSection("Video links", [getMpcVideos(response.videos)])}
+          ${getSection("Videos", [getVideosMultiControl(response.videos)])}
 
           ${getSection("Doc details", [
             getLastUpdated("Last updated", response.lastUpdated),
@@ -52,6 +59,13 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             getThumbnail(response.thumbnail),
           ])}
         `;
+
+        document.querySelectorAll('sp-action-button[href]').forEach((el) => {
+            el.addEventListener('click', (e) => {
+                el.setAttribute('selectedItemText', '');
+                window.open(el.getAttribute('href'), el.getAttribute('target') || null);
+            });
+        });
       });
     }
   );
@@ -67,9 +81,9 @@ function getSection(sectionTitle, lists) {
   if (html) {
     return `<div>
       <p class="spectrum-Heading spectrum-Heading--L spectrum-Heading--light">${sectionTitle}</p>
-      <div>
+      <sp-button-group>
         ${html}
-      </div>
+      </sp-button-group>
     </div>`;
   } else {
     return "";
@@ -84,10 +98,14 @@ function getJira(kts) {
   let html = '';
   
   for (let kt of kts) {
+    /*
     html += `<a class="spectrum-ActionButton spectrum-ActionButton--emphasized"
               href="https://jira.corp.adobe.com/browse/${kt}" target="_blank">
               <span class="spectrum-ActionButton-label">Jira @ KT-${kt}</span>
             </a>`;
+    */
+
+    html += `<sp-action-button href="https://jira.corp.adobe.com/browse/KT-${kt}" target="_blank">Jira @ KT-${kt}</sp-action-button>`
   }
 
   return html;
@@ -100,10 +118,7 @@ function getLastUpdated(title, lastUpdated) {
 
   let m = moment(lastUpdated);
 
-  return  `<button class="spectrum-ActionButton is-selected">
-            <span class="spectrum-ActionButton-label">${title}: ${m.fromNow()} <em>(${m.format("MM/DD/YYYY")})</em></span>
-          </button>`;
-
+  return ` <sp-action-button selected>${title}: ${m.fromNow()} <em>(${m.format("MM/DD/YYYY")})</sp-action-button>`
 }
 
 function getCorpGitEdit(publicGitEdit) {
@@ -116,10 +131,14 @@ function getCorpGitEdit(publicGitEdit) {
     "//git.corp.adobe.com/"
   );
 
+  return `<sp-action-button href="${corpGitEdit}" target="_blank">Adobe Corp Git</sp-action-button>`
+
+  /*
   return `<a class="spectrum-ActionButton spectrum-ActionButton--emphasized"
             href="${corpGitEdit}" target="_blank">
           <span class="spectrum-ActionButton-label">Adobe Corp Git</span>
           </a>`;
+    */          
 }
 
 function getPublicGitEdit(publicGitEdit) {
@@ -127,10 +146,14 @@ function getPublicGitEdit(publicGitEdit) {
     return '';
   }
 
+  return `<sp-action-button href="${publicGitEdit}" target="_blank">Github.com</sp-action-button>`
+
+  /*
   return `<a class="spectrum-ActionButton spectrum-ActionButton--emphasized"
             href="${publicGitEdit}" target="_blank">
           <span class="spectrum-ActionButton-label">Github.com</span>
           </a>`;
+    */          
 }
 
 function getVsCode(contentRoot, gitRepo, filename) {
@@ -141,10 +164,15 @@ function getVsCode(contentRoot, gitRepo, filename) {
   const gitRepoName = gitRepo.substring(gitRepo.lastIndexOf("/") + 1);
   const fileSystemPath = `${contentRoot}${gitRepoName}/${filename}`;
 
+  return `<sp-action-button href="vscode://file/${fileSystemPath}">Open in VS Code</sp-action-button>`
+
+
+  /*
   return `<a class="spectrum-ActionButton spectrum-ActionButton--emphasized"
             href="vscode://file/${fileSystemPath}">
           <span class="spectrum-ActionButton-label">Open in VS Code</span>
           </a>`;
+    */          
 }
 
 function getThumbnail(thumbnailId) {
@@ -152,58 +180,114 @@ function getThumbnail(thumbnailId) {
     return '';
   }
 
-  return `
-    <a class="spectrum-ActionButton spectrum-ActionButton--emphasized"
-              href="https://cdn.experienceleague.adobe.com/thumb/${thumbnailId}" target="thumbnail_${thumbnailId}">
-          <span class="spectrum-ActionButton-label">Thumbnail: ${thumbnailId}</span>
-    </a>
-  `;
+  return `<sp-action-button href="https://cdn.experienceleague.adobe.com/thumb/${thumbnailId}" target="thumbnail_${thumbnailId}">
+             Thumbnail: ${thumbnailId}
+          </sp-action-button>`;
 }
 
-function getMpcVideos(mpcVideoUrls) {
-  let list = "";
 
-  if (!mpcVideoUrls) {
-    return '';
-  }
-
-  for (const mpcVideoUrl of mpcVideoUrls) {
-    const videoIdRegex = /https:\/\/video.tv.adobe.com\/v\/(\d+)[\/?]+.*/gi;
-
-    let videoId = null;
-
-    console.log(videoIdRegex);
-    console.log(mpcVideoUrl);
-    let match = videoIdRegex.exec(mpcVideoUrl);
-
-    console.log(match);
-
-    console.log("----");
-
-    if (match && match.length === 2) {
-      videoId = match[1];
-    } else {
-      console.log("Could not get video id from: " + mpcVideoUrl);
+function getVideos(mpcVideoUrls, renderFunction) {
+    let list = "";
+  
+    if (!mpcVideoUrls) {
+      return '';
     }
-
-    if (videoId && !isNaN(videoId)) {
-      list += `<a class="spectrum-ActionButton spectrum-ActionButton--emphasized"
-                  href="https://publish.tv.adobe.com/search?q=${videoId}" target="mpcVideo_${videoId}">
-                <span class="spectrum-ActionButton-label">${videoId}</span>
-              </a>`;
+  
+    for (const mpcVideoUrl of mpcVideoUrls) {
+      const videoIdRegex = /https:\/\/video.tv.adobe.com\/v\/(\d+)[\/?]+.*/gi;
+  
+      let videoId = null;  
+      let match = videoIdRegex.exec(mpcVideoUrl);
+  
+      if (match && match.length === 2) {
+        videoId = match[1];
+      } else {
+        console.log("Could not get video id from: " + mpcVideoUrl);
+      }
+  
+      if (videoId && !isNaN(videoId)) {
+        list += renderFunction(videoId);
+      } else {
+        console.error("Invalid video Id: " + videoId);
+      }
+    }
+  
+    if (!list) {
+      return `<button class="spectrum-ActionButton is-selected">
+                <span class="spectrum-ActionButton-label">No videos</span>
+              </button>`;
     } else {
-      console.error("Invalid video Id: " + videoId);
+      return list;
+    }
+  }
+  
+
+function getVideosMultiControl(mpcVideoUrls) {
+    let list = "";
+  
+    if (!mpcVideoUrls) {
+      return '';
+    }
+  
+    for (const mpcVideoUrl of mpcVideoUrls) {
+      const videoIdRegex = /https:\/\/video.tv.adobe.com\/v\/(\d+)[\/?]+.*/gi;
+  
+      let videoId = null;  
+      let match = videoIdRegex.exec(mpcVideoUrl);
+  
+      if (match && match.length === 2) {
+        videoId = match[1];
+      } else {
+        console.log("Could not get video id from: " + mpcVideoUrl);
+      }
+  
+      if (videoId && !isNaN(videoId)) {
+        //list += renderFunction(videoId);
+
+            list += `
+            <sp-action-menu placement="top-start">
+                <sp-icon slot="icon" size="xxs" name="ui:ChevronDownSmall"></sp-icon>
+                <span slot="label">MPC @ ${videoId}</span>
+                <sp-menu>
+                    <sp-menu-item href="https://publish.tv.adobe.com/search?q=${videoId}" target="mpcAdminConsole_${videoId}">
+                        MPC Admin console
+                    </sp-menu-item>
+                    <sp-menu-item href="https://video.tv.adobe.com/v/${videoId}/?quality=12&amp;learn=on" target="mpcDirectVideo_${videoId}">
+                        Direct video link
+                    </sp-menu-item>
+                    <sp-menu-item>
+                        Copy '${videoId}' to clipboard
+                    </sp-menu-item>               
+                </sp-menu>
+            </sp-action-menu>
+            `;
+
+      } else {
+        console.error("Invalid video Id: " + videoId);
+      }
+    }
+  
+    if (!list) {
+        return `<sp-action-button selected>No videos on the page</sp-action-button>`;
+    } else {
+      return list;
     }
   }
 
-  if (!list) {
-    return `<button class="spectrum-ActionButton is-selected">
-              <span class="spectrum-ActionButton-label">No videos</span>
-            </button>`;
-  } else {
-    return list;
-  }
+function renderMpcAdminConsoleLinks(videoId) {
+    return `<a class="spectrum-ActionButton spectrum-ActionButton--emphasized"
+                href="https://publish.tv.adobe.com/search?q=${videoId}" target="mpcAdminConsole_${videoId}">
+                    <span class="spectrum-ActionButton-label">${videoId}</span>
+            </a>`;
 }
+
+function renderMpcDirectVideoLinks(videoId) {
+    return `<a class="spectrum-ActionButton spectrum-ActionButton--emphasized"
+                href="https://video.tv.adobe.com/v/${videoId}/?quality=12&amp;learn=on" target="mpcDirectVideo_${videoId}">
+                    <span class="spectrum-ActionButton-label">${videoId}</span>
+            </a>`;
+}
+
 
 function getMeta(title, value, missingValue) {
   if (!value) {
@@ -211,9 +295,7 @@ function getMeta(title, value, missingValue) {
   }
 
   if (value !== null) {
-    return `<button class="spectrum-ActionButton is-selected">
-              <span class="spectrum-ActionButton-label">${title}: ${value}</span>
-            </button>`;
+    return `<sp-action-button selected>${title}: ${value}</sp-action-button>`;
   } else {
     return "";
   }
@@ -231,11 +313,7 @@ function getMetas(title, values, missingValues) {
   }
 
   if (values && values.length > 0) {
-    return `<button class="spectrum-ActionButton is-selected">
-              <span class="spectrum-ActionButton-label">${title}: ${values.join(
-      ", "
-    )}</span>
-            </button>`;
+    return `<sp-action-button selected>${title}: ${values.join(", ")}</sp-action-button>`;
   } else {
     return "";
   }
@@ -253,4 +331,14 @@ function _getOptionsContentFileSystemPath(obj) {
   }
 
   return "";
+}
+
+function _copyToClipboard(text) {
+    let copyEl = document.getElementById("copy-paste-input");
+    copyEl.value = text;
+    copyEl.select();
+    copyEl.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    alert(copyEl.value);
+    //copyEl.value = '';
 }
